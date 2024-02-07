f51195951d4f2cdc1e95e57b5f03746232c968d7
meganbyrd22 <meganbyrd22@gmail.com>
2024-02-07 12:47:26 -0600

    chore: Add Tailwind config feat: Add basic table content

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<header className="App-header">
+          TABLE HEADER
+          </header>*/}
+      <main>
+      <div className="h-24 border-2 border-red text-red">Header Spaceholder</div>
+      <h1 className="flex h-44 w-96 mr-2 p-2 self-center font-bold text-indigo-400 text-4xl border-2 bg-white rounded-3xl">Hello!</h1>
+      <section className='bg-trueGray-100 flex m-6'>
+        <div className='bg-green'>TABLE</div>
+        <div className='text-red'>Users</div>
+        <div>Table Options, add buttons</div>
+        <div className=""> {/*Table header*/} </div>
+          <div className=""> {/*TABLE*/} </div>
+            <div className=""> {/*Table lines */} </div>
+
+        
+
+
+
+      </section>
+
+      </main>
     </div>
   );
 }
