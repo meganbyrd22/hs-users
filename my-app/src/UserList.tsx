6c0fa57ed7a4fa970e0a3c9538e221ab040e6eb0
meganbyrd22 <meganbyrd22@gmail.com>
2024-02-08 12:18:23 -0600

    chore: Adjust spacing for user elements in table

--- my-app/src/UserList.tsx
+++ my-app/src/UserList.tsx
@@ -6,12 +6,7 @@ interface UserListProps {
     selectUser: (user: User) => void;
 }
 
-function updateBirthdayFormat(dateString: string): string {
-    const date = new Date(dateString);
-    const month = date.toLocaleString('default', { month: 'long'});
-    const day = date.getDate();
-    return `${month}: ${day}`;
-}
+
 
 function UserList({ users, selectUser }: UserListProps){
     console.log('Users', users);
@@ -19,16 +14,12 @@ function UserList({ users, selectUser }: UserListProps){
         <ul>
             {users.map((user) => (
                 <li key={user.id} onClick={() => selectUser(user)} className="cursor-pointer">
-                    <div className="grid grid-cols-4 m-2">
-                        <div className="flex gap-4">
-                       <img src={user.image} alt={`${user.firstName} ${user.lastName}`} className="m-2 w-8 h-8 rounded-full" />
-                            <div>
+                    <div className="m-2 flex">
+                        <img src={user.image} alt={`${user.firstName} ${user.lastName}`} className="m-2 w-8 h-8 rounded-full" />
+                            <div className="grid grid-cols-2 m-2 w-2/3">
                                 <div className="text-xl"> {user.firstName} {user.lastName}</div>
                                 <div className="italic text-md"> {user.email} </div>
                             </div>
-                       </div>
-                       <div className="text-xl m-2">{updateBirthdayFormat(user.dob)} </div>
-                       <div className="text-xl m-2">{user.gender.charAt(0).toUpperCase() + user.gender.slice(1)}</div>
                     </div>
                 </li>
       ))}
