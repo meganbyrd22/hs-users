012626fb50a1cb2bc249dab720a2db89e125a399
meganbyrd22 <meganbyrd22@gmail.com>
2024-02-09 14:08:41 -0600

    chore: Style modal form with grid

--- my-app/src/EditUserForm.tsx
+++ my-app/src/EditUserForm.tsx
@@ -29,72 +29,82 @@ function EditUserForm({ user, onSubmit, onClose}: EditFormProps) {
 
 return (
     <div className= "flex fixed justify-center items-center inset-0 bg-black bg-opacity-50">
-            <form onSubmit={handleSubmit} className="h-96 w-96 border-4 border-red  bg-white text-xxl flex flex-col">
-                <h1 className="font-bold text-xxl">Edit User</h1>
-                <label>
-                    First Name: 
-                    <input 
-                        type="text" 
-                        name="firstName" 
-                        value={updatedUser.firstName} 
-                        onChange={handleChange} 
-                    />
-                </label>
+            <form onSubmit={handleSubmit} className="h-2/4 w-2/5 border-4 border-red rounded-lg p-2 bg-white text-xxl flex flex-col">
+                <h1 className="font-bold text-3xl p-2">Edit User</h1>
+                <div className="grid grid-cols-1 gap-2">
+                    <div className="flex">
+                        <label className="m-2">
+                            First Name: 
+                            <input className="border-2 rounded-lg text-sm p-2 w-60 ml-2 italic"
+                                type="text" 
+                                name="firstName" 
+                                value={updatedUser.firstName} 
+                                onChange={handleChange} 
+                            />
+                        </label>
 
-                <label>
-                    Last Name: 
-                    <input 
-                        type="text" 
-                        name="lastName" 
-                        value={updatedUser.lastName} 
-                        onChange={handleChange} 
-                    />
-                </label>
+                        <label className="m-2">
+                            Last Name: 
+                            <input className="border-2 rounded-lg text-sm p-2 w-60 ml-2 italic"
+                                type="text" 
+                                name="lastName" 
+                                value={updatedUser.lastName} 
+                                onChange={handleChange} 
+                            />
+                        </label>
+                    </div>
+                    <div className="grid grid-cols-2">
+                    <label className="m-2">
+                        Email: 
+                        <input className="border-2 rounded-lg text-sm p-2 w-60 ml-12 italic"
+                            type="text" 
+                            name="email" 
+                            value={updatedUser.email} 
+                            onChange={handleChange} 
+                        />
+                    </label>
+                    </div>
+                    <div className="w-full h-6 bg-black bg-opacity-25 rounded-lg"></div>
+                    <div className="mt-4">
+                        <label className="m-2">
+                            Birthday: 
+                            <input className="border-2 rounded-lg text-sm p-2 w-60 ml-7 italic"
+                                type="date" 
+                                name="dob" 
+                                value={updatedUser.dob} 
+                                onChange={handleChange} 
+                            />
+                        </label>
+                        
 
-                <label>
-                    Email: 
-                    <input 
-                        type="text" 
-                        name="email" 
-                        value={updatedUser.email} 
-                        onChange={handleChange} 
-                    />
-                </label>
+                        <label className="m-2">
+                            Gender
+                            <select className="border-2 rounded-lg text-sm p-2 w-60 ml-2 italic"
+                                name="gender" 
+                                value={updatedUser.gender} 
+                                onChange={handleChange}>
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>  
+                            </select>
+                        </label>
+                    </div>
+                    <div className="mt-4">
+                        <label className="m-2">
+                            State: 
+                            <input className="border-2 rounded-lg text-sm p-2 w-60 ml-12 italic"
+                                type="text" 
+                                name="state" 
+                                value={updatedUser.state} 
+                                onChange={handleChange} 
+                            />
+                        </label>
+                    </div>
+                    
 
-                <label>
-                    Date of Birth: 
-                    <input 
-                        type="date" 
-                        name="dob" 
-                        value={updatedUser.dob} 
-                        onChange={handleChange} 
-                    />
-                </label>
-
-                <label>
-                    Gender
-                    <select 
-                        name="gender" 
-                        value={updatedUser.gender} 
-                        onChange={handleChange}>
-                            <option value="male">Male</option>
-                            <option value="female">Female</option>  
-                    </select>
-                </label>
-
-            <label>
-                    State of Residence: 
-                    <input 
-                        type="text" 
-                        name="state" 
-                        value={updatedUser.state} 
-                        onChange={handleChange} 
-                    />
-                </label>
-
-                <button type="button" onClick={onClose}>Close</button>
-
-
+            </div>
+            <div className="flex justify-center">
+                <button className="mt-16 p-2 border-2 rounded-full text-sm w-24"type="button" onClick={onClose}>Close</button>
+            </div>
             </form>
     </div>
     )
