e7b0e3dc542292669e4daa8a5f542346f5e3d9ce
meganbyrd22 <meganbyrd22@gmail.com>
2024-02-13 10:31:58 -0600

    chore: Add logic for timeout of Toast component chore: Add return message and close option

--- my-app/src/Toast.tsx
+++ my-app/src/Toast.tsx
@@ -1,7 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 
-export default function Toast({message, onClose}) {
+interface ToastProps {
+    message: string;
+    onClose: () => void;
+}
+ 
+
+const Toast: React.FC<ToastProps> = ({message, onClose}: ToastProps) => {
+    useEffect(() => {
+        const timeout = setTimeout(() =>{
+            onClose();
+        }, 3000);
+        return () => clearTimeout(timeout);
+    }, [onClose])
+
     return (
-        <div>TOAST POPUP</div>
+        <div>
+            <div>{message}</div>
+            <button className="bg-gray-400" onClick={onClose}>X</button>
+        </div>
     )
 }
+
+export default Toast;
